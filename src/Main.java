510cc7d9f66d88334f34f8edbb08528d6a0a4cc3
tom803 <tom803@users.noreply.github.com>
2021-10-19 20:08:14 +0200

    added Triangle

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 		System.out.println("Hello world.");
 		System.out.println(new Rectangle());
 		System.out.println(new Circle());
-	}	
+		System.out.println(new Triangle());
+	}
 }
